04c4c85070b59e9d0323850a565f63be2d704578
rosstheboss <intern@gstudiobranding.com>
2023-10-27 12:51:56 +0200

    built site

--- index.php
+++ index.php
@@ -8,11 +8,9 @@
     <title>Landing Page</title>
  </head>
  <body>
-    <header class="main-header">
+    <header id="masthead" class="main-header">
         <div class="container flex a-center f-between">
-
             <a href="#" class="logo">Kreate<span>Digital</span></a>
-
             <nav class="main-nav">
                 <button id="menuToggler" class="menu-toggle">
                     <div class="box">
@@ -21,18 +19,15 @@
                         <div class="bar"></div>
                     </div>
                 </button>
-
                 <ul id="menuDropDown" class="menu-items">
                     <li class="item-wrap"><a href="#" class="item">Home</a></li>
                     <li class="item-wrap"><a href="#" class="item">Works</a></li>
                     <li class="item-wrap"><a href="#" class="item">Services</a></li>
                     <li class="item-wrap"><a href="#" class="item">Team</a></li>
                     <li class="item-wrap"><a href="#" class="item">Prices</a></li>
-                    <li class="item-wrap"><a href="#" class="item">Blog</a></li>
                     <li class="item-wrap"><a href="#" class="item">Contact Us</a></li>
                 </ul>
             </nav>
-
         </div>
     </header>
 
@@ -80,6 +75,278 @@
         </div>
     </section>
     
+    <!-- add javascript functionality -->
+    <section class="main-work">
+        <div class="container">
+
+            <header class="section-header">
+                <h2>Our Work</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </header>
+
+            <div class="select-work">
+                <button id="categoryChanger" data-category="all">All</button>
+                <button id="categoryChanger" data-category="design">Design</button>
+                <button id="categoryChanger" data-category="web">Web Developement</button>
+                <button id="categoryChanger" data-category="marketing">Campaign</button>
+                <button id="categoryChanger" data-category="social media">Social Media</button>
+            </div>
+
+
+            <div class="project-grid">
+                <div class="block" aria-label="design">
+                    <span class="project-title">Title</span>
+                    <img src="./public/images/project-1.jpg" alt="Project Title" class="img-fluid">
+                </div>
+
+                <div class="block" aria-label="web">
+                    <span class="project-title">Title</span>
+                    <img src="./public/images/project-1.jpg" alt="Project Title" class="img-fluid">
+                </div>
+
+                <div class="block" aria-label="marketing">
+                    <span class="project-title">Title</span>
+                    <img src="./public/images/project-1.jpg" alt="Project Title" class="img-fluid">
+                </div>
+
+                <div class="block" aria-label="web">
+                    <span class="project-title">Title</span>
+                    <img src="./public/images/project-1.jpg" alt="Project Title" class="img-fluid">
+                </div>
+
+                <div class="block" aria-label="design">
+                    <span class="project-title">Title</span>
+                    <img src="./public/images/project-1.jpg" alt="Project Title" class="img-fluid">
+                </div>
+
+                <div class="block" aria-label="web">
+                    <span class="project-title">Title</span>
+                    <img src="./public/images/project-1.jpg" alt="Project Title" class="img-fluid">
+                </div>
+
+                <div class="block" aria-label="design">
+                    <span class="project-title">Title</span>
+                    <img src="./public/images/project-1.jpg" alt="Project Title" class="img-fluid">
+                </div>
+
+                <div class="block" aria-label="web">
+                    <span class="project-title">Title</span>
+                    <img src="./public/images/project-1.jpg" alt="Project Title" class="img-fluid">
+                </div>
+
+                <div class="block" aria-label="social media">
+                    <span class="project-title">Title</span>
+                    <img src="./public/images/project-1.jpg" alt="Project Title" class="img-fluid">
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <Section class="main-services">
+        <div class="container">
+            <header class="section-header">
+                <h2>Our Services</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </header>
+            
+            <div class="grid">
+            <div class="usp-block">
+                    <img src="./public/elements/bolt.svg" alt="USP Icon" class="icon">
+                    <h4>USP Heading</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci culpa maxime alias provident beatae eligendi itaque sed explicabo</p>
+                </div>
+                <div class="usp-block">
+                    <img src="./public/elements/bolt.svg" alt="USP Icon" class="icon">
+                    <h4>USP Heading</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci culpa maxime alias provident beatae eligendi itaque sed explicabo</p>
+                </div>
+                <div class="usp-block">
+                    <img src="./public/elements/bolt.svg" alt="USP Icon" class="icon">
+                    <h4>USP Heading</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci culpa maxime alias provident beatae eligendi itaque sed explicabo</p>
+                </div>
+                <div class="usp-block">
+                    <img src="./public/elements/bolt.svg" alt="USP Icon" class="icon">
+                    <h4>USP Heading</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci culpa maxime alias provident beatae eligendi itaque sed explicabo</p>
+                </div>
+                <div class="usp-block">
+                    <img src="./public/elements/bolt.svg" alt="USP Icon" class="icon">
+                    <h4>USP Heading</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci culpa maxime alias provident beatae eligendi itaque sed explicabo</p>
+                </div>
+                <div class="usp-block">
+                    <img src="./public/elements/bolt.svg" alt="USP Icon" class="icon">
+                    <h4>USP Heading</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci culpa maxime alias provident beatae eligendi itaque sed explicabo</p>
+                </div>
+            </div>
+        </div>
+    </Section>
+
+    <section class="parallax">
+        <div class="container flex a-center f-center">
+            <p>"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore explicabo expedita, tenetur quis eligendi illo sint, provident voluptatibus tempore fugit, sit quam optio facere natus aperiam. Fugit molestiae sed rem."</p>
+        </div>
+    </section>
+
+    <section class="main-team">
+        <div class="container">
+            <header class="section-header">
+                <h2>Our Team</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </header>
+
+            <div class="grid">
+                <div class="block">
+                    <img src="./public/images/team-image.jpg" alt="Person Name - Occupation" class="img-fluid">
+                    <h4>Person Name</h4>
+                    <p>Occupation</p>
+
+                    <div class="socials">
+                        <a href="#">Insta</a>
+                        <a href="#">LinkedIn</a>
+                        <a href="#">Twitter</a>
+                        <a href="#">facebook</a>
+                    </div>
+                </div>
+                <div class="block">
+                    <img src="./public/images/team-image.jpg" alt="Person Name - Occupation" class="img-fluid">
+                    <h4>Person Name</h4>
+                    <p>Occupation</p>
+
+                    <div class="socials">
+                        <a href="#">Insta</a>
+                        <a href="#">LinkedIn</a>
+                        <a href="#">Twitter</a>
+                        <a href="#">facebook</a>
+                    </div>
+                </div>
+                <div class="block">
+                    <img src="./public/images/team-image.jpg" alt="Person Name - Occupation" class="img-fluid">
+                    <h4>Person Name</h4>
+                    <p>Occupation</p>
+
+                    <div class="socials">
+                        <a href="#">Insta</a>
+                        <a href="#">LinkedIn</a>
+                        <a href="#">Twitter</a>
+                        <a href="#">facebook</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="main-pricing">
+        <div class="container">
+            <header class="section-header">
+                <h2>How Much</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </header>
+            <div class="flex-wrap f-evenly">
+                <div class="pricing-card">
+                    <h3>Basic</h3>
+                    <div class="card-body">
+                        <p>$29.99</p>
+                        <ul class="">
+                            <li>2 Domains</li>
+                            <li>5GB Hosting</li>
+                            <li>Limited Accounts</li>
+                            <li>FTP</li>
+                            <li>Full Support</li>
+                        </ul>
+                        <button>Buy Now</button>
+                    </div>
+                </div>
+                <div class="pricing-card">
+                    <h3>Standard</h3>
+                    <div class="card-body">
+                        <p>$59.99</p>
+                        <ul class="">
+                            <li>5 Domains</li>
+                            <li>25GB Hosting</li>
+                            <li>Unlimited Accounts</li>
+                            <li>FTP</li>
+                            <li>Full Support</li>
+                        </ul>
+                        <button>Buy Now</button>
+                    </div>
+                </div>
+                <div class="pricing-card">
+                    <h3>Pro</h3>
+                    <div class="card-body">
+                        <p>$99.99</p>
+                        <ul class="">
+                            <li>40 Domains</li>
+                            <li>500GB Hosting</li>
+                            <li>Unlimited Accounts</li>
+                            <li>FTP & SSH</li>
+                            <li>24/7 Full Support</li>
+                        </ul>
+                        <button>Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="main-contact">
+        <div class="container">
+            <header class="section-header">
+                <h2>Contact Us</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </header>
+
+            <div class="contact-details flex-wrap  f-around">
+                <div class="detail">
+                    <h4>Address</h4>
+                    <p>Some address street,<br> some area</p>
+                </div>
+                <div class="detail">
+                    <h4>E-mail</h4>
+                    <a href="someemail@gmail.com">someemail@gmail.com</a>
+                    <a href="someemail@gmail.com">someemail@gmail.com</a>
+                </div>
+                <div class="detail">
+                    <h4>Phone</h4>
+                    <p>123-456-7890</p>
+                    <p>123-456-7890</p>
+                </div>
+            </div>
+
+            <form action="" class="contact-form">
+                <div class="form-group">
+                    <input type="text" name="" id="" placeholder="Name">
+                    <input type="Email" name="" id="" placeholder="Email">
+                </div>
+                <input type="text" name="" id="" placeholder="Subject">
+                <textarea name="" id="" cols="30" rows="5" placeholder="Message"></textarea>
+                <button>Send Message</button>
+            </form>
+        </div>
+    </section>
+
+    <section class="mapbox">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27503048.087777734!2d5.8629617951807!3d-32.70450425120123!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1c34a689d9ee1251%3A0xe85d630c1fa4e8a0!2sSouth%20Africa!5e0!3m2!1sen!2sza!4v1698391644183!5m2!1sen!2sza" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </section>
+
+    <footer class="main-footer">
+        <div class="container flex a-center d-col d-md-row f-md-between">
+            <a href="#" class="logo">Kreate<span>Digital</span></a>
+
+            <ul class="footer-links">
+                <li class="link-wrap"><a href="#" class="link">Link</a></li>
+                <li class="link-wrap"><a href="#" class="link">Link</a></li>
+                <li class="link-wrap"><a href="#" class="link">Link</a></li>
+                <li class="link-wrap"><a href="#" class="link">Link</a></li>
+                <li class="link-wrap"><a href="#" class="link">Link</a></li>
+            </ul>
+
+            <div class="copyright">Copyright &copy; Ross Simpson 2023</div>
+        </div>
+    </footer>
 
     <script src="./public/script.js"></script>
  </body>
--- public/script.js
+++ public/script.js
@@ -1,7 +1,46 @@
 const menuToggler = document.getElementById('menuToggler');
 const menuDropDown = document.getElementById('menuDropDown');
 
+const scrollY = window.scrollY;
+const header = document.getElementById('masthead');
+
 // Activate mobile menu dropdown
 menuToggler.addEventListener('click', () =>{
     menuDropDown.classList.toggle('menu-items--menu-active');
 });
+
+// change header colors
+window.addEventListener('scroll', () => {
+    scrollY > "1" 
+    ? header.classList.add('main-header--scroll')
+    : header.classList.remove('main-header--scroll')
+});
+
+
+const categoryChangerBtn = document.querySelectorAll('#categoryChanger');
+
+
+for (let i = 0; i < categoryChangerBtn.length; i++) {
+    categoryChangerBtn[i].addEventListener('click', (e) => {
+        let a = e.target;
+        
+        if (a.hasAttribute("data-category")) {
+            const selectedCategory = a.getAttribute("data-category");
+            const allLabels = document.querySelectorAll('[aria-label]');
+
+            for (let i = 0; i < allLabels.length; i++) {
+                const label = allLabels[i];
+                const labelCategory = label.getAttribute('aria-label');
+                
+                if (selectedCategory === 'all') {
+                    label.classList.remove('block--scale-grey');
+                } else if (labelCategory === selectedCategory) {
+                    label.classList.remove('block--scale-grey');
+                } else {
+                    label.classList.add('block--scale-grey');
+                }
+            }
+        }
+    });
+}
+
